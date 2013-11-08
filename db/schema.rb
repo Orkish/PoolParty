40e06188e6cfd6cb7a101247e6ec1bd5709beec8
whyu <wesleyhl.yu@gmail.com>
2013-11-08 14:45:02 -0500

    Added signout button

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131108154431) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: true do |t|
     t.string   "username",                            null: false
     t.string   "firstname",                           null: false
