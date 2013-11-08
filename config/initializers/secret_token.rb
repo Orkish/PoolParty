1dcbd2afa6164389b4ccf6446967562aafcc7967
whyu <wesleyhl.yu@gmail.com>
2013-11-08 09:57:08 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poolparty::Application.config.secret_key_base = '2177f22cde6a04880d1f0f28f7a6a489d2f8f1db0eb6463616f4ce56e27d640414c3bcfb50619750692b9bfba90300545fc044d99038969886b974c665ed80b1'
