a27a1a9e8a924c85ea6effaa51e7914c0bc484ac
Orkish <winson.yuan@hotmail.com>
2013-11-09 23:17:32 -0500

    updated splash page

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,3 +1,30 @@
+var $scoll_position,
+    $height,
+    $button_aboutus;
+
+
+function shiftOne() {
+    $doc.animate({scrollTop: $height/3 });
+}
+
+function shiftTwo() {
+    $doc.animate({scrollTop: $height });
+}
+
+$(function() {
+  $doc = $('html, body');
+  $height = $(document).height();
+  $button_aboutus = $('#button-aboutus');
+  $button_signup = $('#button-signup');
+
+  $button_aboutus.on("click", shiftOne);
+  $button_signup.on("click", shiftTwo);
+});
+
+
+
+
+////// Javier's code /////////
 function updateSize() { 
   var windowW = $(window).width(); 
   var windowH = $(window).height(); 
