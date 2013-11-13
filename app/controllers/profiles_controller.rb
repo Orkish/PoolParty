2744e70af049333f765f200fe7c185c7683b3692
whyu <wesleyhl.yu@gmail.com>
2013-11-12 21:35:45 -0500

    changed twilio message

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -33,7 +33,7 @@ class ProfilesController < ApplicationController
     driver = trip_owner.username
     passenger_number = current_user.phone
     @client = Twilio::REST::Client.new ENV['TWIL_ID'], ENV['TWIL_TOKEN']
-    @message = @client.account.messages.create({:to => "+1"+"#{number}", :from => "+13475805712", :body => "Howdy! Please Contact #{driver} at #{passenger_number}"})
+    @message = @client.account.messages.create({:to => "+1"+"#{number}", :from => "+13475805712", :body => "Howdy! Please Contact #{current_user} at #{passenger_number}"})
     respond_to do |format|
       format.json {render :json => {driver: driver, phone: number}}
     end
